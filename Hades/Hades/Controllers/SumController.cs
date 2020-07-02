2b7d3add63aedcb69f5adb2d39bbca75d1ac80b0
Pavel Skala <pavlass2@gmail.com>
2020-07-02 17:38:47 +0200

    sum controller

---
+++ Hades/Hades/Controllers/SumController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hades.Controllers
+{    
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class SumController : ControllerBase
+    {
+        private readonly ILogger<SumController> _logger;
+
+        public SumController(ILogger<SumController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public int Add(int a, int b)
+        {
+            _logger.LogInformation("Scitam " + a + " + " + b);
+            return a + b;
+        }
+    }
+}
