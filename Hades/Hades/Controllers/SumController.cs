28238ecf126dd5cd02de00d2af6fc8c489306ad1
Pavel Skala <pavlass2@gmail.com>
2020-07-02 22:31:26 +0200

    "add" operation backend implemented

--- Hades/Hades/Controllers/SumController.cs
+++ Hades/Hades/Controllers/SumController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,10 +23,46 @@ namespace Hades.Controllers
         }
 
         [HttpPost]
-        public int Add(int a, int b)
+        public int? Add(JsonElement values)
         {
-            _logger.LogInformation("Scitam " + a + " + " + b);
-            return a + b;
+            // Input variables
+            int? input1 = null;
+            int? input2 = null;
+            try
+            {
+                // Unwrap into separate JSONs.
+                JsonElement jsonInput1 = values.GetProperty("input1");
+                JsonElement jsonInput2 = values.GetProperty("input2");
+
+                // Unwrap into integers.
+                input1 = jsonInput1.GetInt32();
+                input2 = jsonInput2.GetInt32();
+            }
+            catch(KeyNotFoundException ex)
+            {
+                _logger.LogError(ex, "Paremeters passed from frontend are not correctly named.", values);
+            }
+            catch(InvalidOperationException ex)
+            {
+                _logger.LogError(ex, "Paremeters passed from frontend are of a wrong kind.", values);
+            }
+            catch(FormatException ex)
+            {
+                _logger.LogError(ex, "Paremeters passed from frontend are not Int32.", values);
+            }     
+
+            if(input1 != null && input2 != null)
+            {
+                // If everything went smoothly, return result.
+                _logger.LogInformation("Scitam " + input1 + " + " + input2 + " = " + (input1 + input2));
+                return input1 + input2;
+            }
+            else
+            {
+                // Log errors and return null.
+                _logger.LogWarning("One or multiple errors have occurred during \"add\" operation.");
+                return null;
+            }            
         }
 
         public string Index()
