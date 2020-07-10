6a3e4eb0c526f0e89fa78c448f57ee90e97153d2
Pavel Skala <pavlass2@gmail.com>
2020-07-10 21:32:40 +0200

    BACKEND: Added ApplicationUser.Id to response

---
+++ Hades/Hades/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hades.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public ApplicationUser Author { get; set; }
+        public Group PostedInGroup { get; set; }
+        public string TextContent { get; set; }
+    }
+}
