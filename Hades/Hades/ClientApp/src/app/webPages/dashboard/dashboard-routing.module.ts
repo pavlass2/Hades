965d3c8e5df6df09a7a6330ab09418934c44a201
Rosta6 <rostislav.kubin1@gmail.com>
2020-07-13 12:57:49 +0200

    rouer guards implemented

--- Hades/Hades/ClientApp/src/app/webPages/dashboard/dashboard-routing.module.ts
+++ Hades/Hades/ClientApp/src/app/webPages/dashboard/dashboard-routing.module.ts
@@ -5,16 +5,21 @@ import { CreateGroupComponent } from './components/create-group/create-group.com
 import { JoinGroupComponent } from './components/join-group/join-group.component';
 import { UserNameComponent } from './components/user-name/user-name.component';
 import { MainRoomComponent } from '../groupRoom/components/main-room/main-room.component';
+import { InfoSetGuard } from 'src/app/core/guards/info-set.guard';
+import { NameSetGuard } from 'src/app/core/guards/name-set.guard';
+
+
 
 
 const routes: Routes = [
   { path: "main", component:  MainMenuComponent},
   { path: "main/createGroup", component:  CreateGroupComponent},
   { path: "main/joinGroup", component:  JoinGroupComponent},
-  { path: "main/userName", component:  UserNameComponent},
+  { path: "main/userName", component:  UserNameComponent, canActivate: [NameSetGuard]},
 
    
-  { path: "mainRoom", component: MainRoomComponent}
+  { path: "mainRoom", component: MainRoomComponent, canActivate: [InfoSetGuard]},
+  { path: '',   redirectTo: '/main', pathMatch: 'full' }
 ];
 
 @NgModule({
