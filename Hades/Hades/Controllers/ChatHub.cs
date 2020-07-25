7ff539f4d26cf8ef9708debdaf03d531d474adff
Pavel Skala <pavlass2@gmail.com>
2020-07-25 18:59:16 +0200

    BACKEND: #11 ChatHub.ConnectClient method implementation + Method ChatHub.ConnectClient updated - every client calling this method or subscribing to "ReceiveMessage"  needs to register first by calling ChatHub.ConnectClient.

--- Hades/Hades/Controllers/ChatHub.cs
+++ Hades/Hades/Controllers/ChatHub.cs
@@ -14,10 +14,12 @@ using System.Threading.Tasks;
 namespace Hades.Controllers
 {    public class ChatHub : Hub
     {
-        DbDataProvider dbDataProvider;
-        UserManager<ApplicationUser> userManager;
-        ILogger<ChatHub> logger;
-        ControllerUtils controllerUtils;
+        private DbDataProvider dbDataProvider;
+        private UserManager<ApplicationUser> userManager;
+        private ILogger<ChatHub> logger;
+        private ControllerUtils controllerUtils;
+        private static Dictionary<string, string> connectionLibrary = new Dictionary<string, string>();
+
         public ChatHub(
             DbDataProvider dbDataProvider,
             UserManager<ApplicationUser> userManager,
@@ -30,6 +32,12 @@ namespace Hades.Controllers
             this.logger = logger;
             this.controllerUtils = controllerUtils;
         }
+
+        /// <summary>
+        /// Sends message to other group members and stores it to the DB. Before this method is called, userId needs to be added to the "connectionLibrary" which is done by calling "ConnectClient".
+        /// </summary>
+        /// <param name="requestData">"message" - the text of the message; "userId" of the user sending message; ""</param>
+        /// <returns></returns>
         public async Task SendMessage(JsonElement requestData)
         {
             Dictionary<string, Type> input = new Dictionary<string, Type> {
@@ -47,18 +55,26 @@ namespace Hades.Controllers
 
                 if (author != null)
                 {
-                    Group group = dbDataProvider.GetGroup((string)result["groupName"]);
-                    if (group != null)
+                    if (connectionLibrary.ContainsKey(Context.ConnectionId))
                     {
-                        string textContent = (string)result["message"];
-                        await Clients.All.SendAsync("ReceiveMessage", new JsonResult(new { Message = textContent, NickName = author.NickName, UserId = author.Id }));
+                        Group group = dbDataProvider.GetGroup((string)result["groupName"]);
+                        if (group != null)
+                        {
+                            string textContent = (string)result["message"];
+                            await Clients.Group(group.Name).SendAsync(
+                                "ReceiveMessage", new JsonResult(new { Message = textContent, NickName = author.NickName, UserId = author.Id }));
 
-                        await dbDataProvider.AddMessageAsync(new Message(author, group, textContent, DateTime.Now, (string)result["date"]));
-                        logger.LogInformation("\"SendMessage\" processing for group " + group.Name + " successful.");
+                            await dbDataProvider.AddMessageAsync(new Message(author, group, textContent, DateTime.Now, (string)result["date"]));
+                            logger.LogInformation("\"SendMessage\" processing for group " + group.Name + " successful.");
+                        }
+                        else
+                        {
+                            logger.LogError("Error occurred during \"SendMessage\" processing: Group was NOT found.");
+                        }
                     }
                     else
                     {
-                        logger.LogError("Error occurred during \"SendMessage\" processing: Group was NOT found.");
+                        logger.LogError("Error occurred during \"SendMessage\" processing: Message author was not found in \"connectionLibrary\"");
                     }
                 }
                 else
@@ -68,7 +84,52 @@ namespace Hades.Controllers
             }
             else
             {
-                logger.LogError("Error occurred during \"SendMessage\" processing.");
+                logger.LogError("Error occurred during \"SendMessage\" processing. Wrong parameters.");
+            }
+        }
+
+        /// <summary>
+        /// Adds client to a group that will be notified about group events. This needs to be called before SendMessage method is called.
+        /// </summary>
+        /// <param name="requestData">"userId" of the user and "groupName" that client is subscribing to.</param>
+        /// <returns></returns>
+        public async Task ConnectClient(JsonElement requestData)
+        {
+            Dictionary<string, Type> input = new Dictionary<string, Type> {
+                { "userId", typeof(string) },
+                { "groupName", typeof(string) }
+            };
+            Dictionary<string, object> result = controllerUtils.UnwrapJsonRequest(input, requestData);
+
+            if (result != null)
+            {
+                string userId = (string)result["userId"];
+                string groupName = (string)result["groupName"];
+
+                ApplicationUser applicationUser = await userManager.FindByIdAsync(userId);
+
+                if (applicationUser != null)
+                {
+                    // Add to group so client will be notified about new users and messages
+                    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+                    // If this is the first connection of this user, notify other clients about it
+                    if (!(connectionLibrary.ContainsKey(Context.ConnectionId)))
+                    {
+                        await Clients.OthersInGroup(groupName).SendAsync("NewUserConnected", new JsonResult(new { UserName = applicationUser.NickName }));
+                    }
+
+                    // Add the connection to the library so we can work with it later
+                    connectionLibrary.Add(Context.ConnectionId, groupName);
+                }
+                else
+                {
+                    logger.LogError("Error occurred during \"ConnectClient\" processing. User trying to connect was not found in DB.");
+                }
+            }
+            else
+            {
+                logger.LogError("Error occurred during \"ConnectClient\" processing. Wrong parameters.");
             }
         }
     }
